68fd65b0ddf95f8ac87360ebd9b5a7c454d94bbd
Josphat Muriu <Muriujosphatt@gmail.com>
2023-06-21 16:39:14 +0300

    file_path directory

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ if (process.env.NODE_ENV === "production") {
 
   app.get("*", (req, res) =>
     res.sendFile(
-      path.resolve(__dirname, "../", "client", "build", "index.html")
+      path.resolve(__dirname, "../", "client", "public", "index.html")
     )
   );
 } else {
